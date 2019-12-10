2bae1ecb4b9bdb4e319282bc36863d97f0333f59
Michał Regulski <regulskimichal@outlook.com>
2019-12-10 15:08:59 +0100

    Including pytorch+collect_env.py, requirements.txt

--- src/main.py
+++ src/main.py
@@ -3,15 +3,19 @@ import os
 from matplotlib import image
 from pandas import read_csv
 
+path = 'D:\\dataset'
 
-if __name__ == '__main__':
-    path = '/home/mregulski/dataset'
+
+def load():
     loaded_images = {}
     for filename in os.listdir(path):
-        if os.path.isdir(path + '/' + filename) and filename == 's01_l01':
-            for image_filename in os.listdir(path + '/' + filename):
-                loaded_images[filename + '/' + image_filename] = image.imread(path + '/' + filename + '/' + image_filename)
-    table = read_csv(path + '/trainVal.csv')
-    print(loaded_images['s01_l01/1_1.png'])
-    print(len(loaded_images))
-    print(table)
+        if os.path.isdir(path + os.path.sep + filename):
+            for image_filename in os.listdir(path + os.path.sep + filename):
+                image_path = path + os.path.sep + filename + os.path.sep + image_filename
+                loaded_images[filename, image_filename] = image.imread(image_path)
+    loaded_table = read_csv(path + os.path.sep + 'trainVal.csv')
+    return loaded_images, loaded_table
+
+
+if __name__ == '__main__':
+    images, tables = load()
