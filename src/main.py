7a6c81c5b69e9658cde2472248f0db2e461eb3ad
Piotr <valadin96@gmail.com>
2020-01-11 14:05:51 +0100

    Initial commit

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import os
+import matplotlib.pyplot as plt
+import cv2
 
+from src.segmentation import segment_license_plate, segment_characters
 from matplotlib import image
 from pandas import read_csv
+from skimage import img_as_ubyte
 
-path = 'D:\\dataset'
+
+
+path = 'C:\\dataset'
 
 
 def load():
@@ -13,9 +19,23 @@ def load():
             for image_filename in os.listdir(path + os.path.sep + filename):
                 image_path = path + os.path.sep + filename + os.path.sep + image_filename
                 loaded_images[filename, image_filename] = image.imread(image_path)
+
     loaded_table = read_csv(path + os.path.sep + 'trainVal.csv')
     return loaded_images, loaded_table
 
 
 if __name__ == '__main__':
     images, tables = load()
+    for image in images.values():
+
+        img = img_as_ubyte(image)
+        plate = segment_license_plate(img)
+        plt.imshow(plate)
+
+        chars = segment_characters(plate)
+        print(len(chars))
+
+
+
+
+
